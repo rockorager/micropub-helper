2a6cac1efafc7b126b628b799488daff8064dfd2
Tim Culverhouse <tim@timculverhouse.com>
2021-06-27 08:08:33 -0500

    MVP

--- src/micropub-helper.js
+++ src/micropub-helper.js
@@ -1,52 +1,48 @@
 const qs = require("querystring");
 const axios = require("axios");
+const { Base64 } = require("js-base64");
+const multipartParse = require("../vendor/multipart-form-parser");
+const fs = require('fs');
 
 module.exports = async function (event, tokenUrl) {
-	
+	if (event.isBase64Encoded) {
+		event.body = Base64.atob(event.body);
+	}
+
 	let jsonData = {};
-
-	// Look for query stuff first
-	if (event.multiValueHeaders["Content-Type"][0] === "application/json") {
-		// Just return the JSON, it's already an mf2 object
-		jsonData = JSON.parse(event.body);
-	} else {
-		jsonData = {
-			type: [],
-			properties: {}
-		};
+	let formData = {};
+	if(event.multiValueHeaders.hasOwnProperty("Content-Type")){
+		if (event.multiValueHeaders["Content-Type"][0] === "application/json") {
+			// Just return the JSON, it's already an mf2 object
+			jsonData = JSON.parse(event.body);
+		} else if (event.multiValueHeaders["Content-Type"][0].search("multipart/form-data") > -1){
+			formData = multipartParse.readRequestDataInMemory(event);
+			jsonData = {
+				type: [],
+				properties: {}
+			};
+			// TODO this part should be in implementation side I guess?
+			var buf = Buffer.from(formData.files[0].photo, 'latin1');
+			fs.writeFile('image.jpeg', buf, function(err){
+				//do nothing
+			});
+		} else {
+			// form encoded
+			formData = qs.parse(event.body);
+			// Turn formData into an actual object instead of null
+			formData = JSON.parse(JSON.stringify(formData));
+			jsonData = {
+				type: [],
+				properties: {}
+			};
+		}
 	}
 
-	let formData = qs.parse(event.body);
 
-	// Auth
-	if (
-		!event.headers.hasOwnProperty("authorization") &&
-		!formData.hasOwnProperty("access_token")
-	) {
-		return {
-			statusCode: 401,
-			body: "No access token provided",
-		};
-	}
-	const token = event.headers.authorization || "Bearer " + data.access_token;
-	let res = {};
-	try {
-		res = await axios.get("https://tokens.indieauth.com/token", {
-			headers: {
-				Accept: "application/json",
-				Authorization: token,
-			},
-		});
-	} catch {
-		return {
-			statusCode: 403,
-			body: "Bad token",
-		};
-	}
+	
 
-	// TODO Check for scopes
 
-	// Look for query string stuff
+// Look for query string stuff
 	if (event.queryStringParameters.hasOwnProperty("q")) {
 		switch (event.queryStringParameters.q) {
 			case "config":
@@ -61,7 +57,8 @@ module.exports = async function (event, tokenUrl) {
 					statusCode: 200,
 					body: {},
 					query: 'source',
-					source: 'PUT THE URL HERE'
+					source: event.queryStringParameters.url,
+					properties: event.multiValueQueryStringParameters.properties
 				};
 			case "syndicate-to":
 				//return syndication targets
@@ -73,6 +70,43 @@ module.exports = async function (event, tokenUrl) {
 		}
 	}
 
+
+	// Auth
+	if (
+		!event.headers.hasOwnProperty("authorization") &&
+		!formData.hasOwnProperty("access_token")
+	) {
+		return {
+			statusCode: 401,
+			body: "No access token provided",
+		};
+	}
+	const token = event.headers.authorization || "Bearer " + data.access_token;
+	let res = {};
+	try {
+		res = await axios.get(tokenUrl, {
+			headers: {
+				Accept: "application/json",
+				Authorization: token,
+			},
+		});
+	} catch {
+		return {
+			statusCode: 403,
+			body: "Bad token",
+		};
+	}
+
+	if (res.status === 400 || res.status === 401 || res.status === 403) {
+		return {
+			statusCode: 403,
+			body: "Bad token",
+		};
+	}
+	// Authorized, check scopes
+	const scopes = res.data.scope.split(" ");
+
+	
 	// Look for query stuff first
 	if (event.multiValueHeaders["Content-Type"][0] === "application/json") {
 		// Just return the JSON, it's already an mf2 object
@@ -85,8 +119,10 @@ module.exports = async function (event, tokenUrl) {
 			jsonData.type = ["h-" + formData[key]];
 		} else if (key === "access_token") {
 			//Do nothing
+		} else if (key === "files") {
+			jsonData.files = formData.files;
 		} else {
-			jsonData.properties[key] = formData[key];
+			jsonData.properties[key] = [formData[key]];
 		}
 	});
 
--- test/micropub-helper.test.js
+++ test/micropub-helper.test.js
@@ -1,5 +1,6 @@
 const mphelper = require('../src/micropub-helper');
 var assert = require('assert');
+const nock = require('nock');
 
 // Data
 const article = require('./data/article-with-photo.quill.json');
@@ -10,56 +11,138 @@ const bookmark = require('./data/bookmark.quill.json');
 const like = require('./data/like.quill.json');
 const noteWithPhoto = require('./data/note-with-photo.quill.json');
 const badToken = require('./data/bad-token.json');
-const noToken = require('./data/bad-token.json');
+const noToken = require('./data/noToken.json');
 describe('micropub-helper', function() {
   
 	it('should return an article object', async function() {
+		const data = {
+		  "me": "https://www.timculverhouse.com/",
+		  "client_id": "https://11ndieweb.com",
+		  "scope": "create update delete",
+		  "issued_at": 1399155608,
+		  "nonce": 501884823
+		};
+
+		const scope = nock('https://tokens.indieauth.com')
+	      .get('/token')
+	      .reply(200, data);
 	    const expected=['h-entry'];
 	    const actual = await mphelper(article,'https://tokens.indieauth.com/token');
 	    assert.deepEqual(actual.type,expected);
   	});
 	it('should return a note object', async function() {
+		const data = {
+		  "me": "https://www.timculverhouse.com/",
+		  "client_id": "https://11ndieweb.com",
+		  "scope": "create update delete",
+		  "issued_at": 1399155608,
+		  "nonce": 501884823
+		};
+
+		const scope = nock('https://tokens.indieauth.com')
+	      .get('/token')
+	      .reply(200, data);
 	    const expected=['h-entry'];
 	    const actual = await mphelper(note,'https://tokens.indieauth.com/token');
 	    assert.deepEqual(actual.type,expected);
   	});
 	it('should return a config object', async function() {
+		const data = {
+		  "me": "https://www.timculverhouse.com/",
+		  "client_id": "https://11ndieweb.com",
+		  "scope": "create update delete",
+		  "issued_at": 1399155608,
+		  "nonce": 501884823
+		};
+
+		const scope = nock('https://tokens.indieauth.com')
+	      .get('/token')
+	      .reply(200, data);
 	    const actual = await mphelper(config,'https://tokens.indieauth.com/token');
 	    assert.equal(actual.statusCode,200);
   	});
 	it('should return an event object', async function() {
+		const data = {
+		  "me": "https://www.timculverhouse.com/",
+		  "client_id": "https://11ndieweb.com",
+		  "scope": "create update delete",
+		  "issued_at": 1399155608,
+		  "nonce": 501884823
+		};
+
+		const scope = nock('https://tokens.indieauth.com')
+	      .get('/token')
+	      .reply(200, data);
 	    const expected=['h-event'];
 	    const actual = await mphelper(event,'https://tokens.indieauth.com/token');
 	    assert.deepEqual(actual.type,expected);
   	});
 	it('should return a bookmark object', async function() {
+		const data = {
+		  "me": "https://www.timculverhouse.com/",
+		  "client_id": "https://11ndieweb.com",
+		  "scope": "create update delete",
+		  "issued_at": 1399155608,
+		  "nonce": 501884823
+		};
+
+		const scope = nock('https://tokens.indieauth.com')
+	      .get('/token')
+	      .reply(200, data);
 	    const actual = await mphelper(bookmark,'https://tokens.indieauth.com/token');
 	    assert(actual.properties.hasOwnProperty('bookmark-of'));
   	});
 	it('should return a like object', async function() {
+		const data = {
+		  "me": "https://www.timculverhouse.com/",
+		  "client_id": "https://11ndieweb.com",
+		  "scope": "create update delete",
+		  "issued_at": 1399155608,
+		  "nonce": 501884823
+		};
+
+		const scope = nock('https://tokens.indieauth.com')
+	      .get('/token')
+	      .reply(200, data);
 	    const actual = await mphelper(like,'https://tokens.indieauth.com/token');
 	    assert(actual.properties.hasOwnProperty("like-of"));
   	});
 	it('should return a note with photo object', async function() {
-	    const expected="event";
+		const data = {
+		  "me": "https://www.timculverhouse.com/",
+		  "client_id": "https://11ndieweb.com",
+		  "scope": "create update delete",
+		  "issued_at": 1399155608,
+		  "nonce": 501884823
+		};
+
+		const scope = nock('https://tokens.indieauth.com')
+	      .get('/token')
+	      .reply(200, data);
+	    const expected='image/jpeg';
 	    const actual = await mphelper(noteWithPhoto,'https://tokens.indieauth.com/token');
-	    assert.deepEqual(actual,expected);
-  	});
-  	it('should handle a photo as URL', async function() {
-	    // TODO make data for this one
-	    const expected="event";
-	    const actual = await mphelper(note,'https://tokens.indieauth.com/token');
-	    console.log(actual);
-	    assert.deepEqual(actual,expected);
+	    assert.deepEqual(actual.files[0]['Content-Type'],expected);
   	});
   	it('should reject if bad token', async function() {
-	    const actual = await mphelper(badToken,'https://tokens.indieauth.com/token');
-	    console.log(actual);
+		const scope = nock('https://tokens.indieauth.com')
+	      .get('/badtoken')
+	      .reply(401, "Bad token");
+	    const actual = await mphelper(badToken,'https://tokens.indieauth.com/badtoken');
 	    assert.equal(actual.statusCode,403);
   	});
    	it('should reject if no token', async function() {
-	    const actual = await mphelper(noToken,'https://tokens.indieauth.com/token');
-	    console.log(actual);
+   		const data = {
+		  "me": "https://www.timculverhouse.com/",
+		  "client_id": "https://11ndieweb.com",
+		  "scope": "create update delete",
+		  "issued_at": 1399155608,
+		  "nonce": 501884823
+		};
+
+		const scope = nock('https://tokens.indieauth.com')
+	      .get('/noToken')
+	      .reply(200, data);
+	    const actual = await mphelper(noToken,'https://tokens.indieauth.com/noToken');
 	    assert.equal(actual.statusCode,401);
   	});
 });
